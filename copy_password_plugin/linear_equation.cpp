fd8b50205b5cd72cada7379334bf5adf88c597e4
JinPeng <66198683+jjkbb123@users.noreply.github.com>
2022-04-14 17:09:41 +0800

    Update linear_equation.cpp

--- copy_password_plugin/linear_equation.cpp
+++ copy_password_plugin/linear_equation.cpp
@@ -51,10 +51,20 @@
 
 using namespace std;
 
+const int initLine = 3;
+void showMatrix(int arr[initLine][initLine], int size);
+
+void showMatrix(int arr[initLine][initLine], int size) {
+  for(int i = 0; i < size; i++) {
+    for(int j = 0; j < size; j++) {
+      cout << arr[i][j] << " ";
+    };
+    cout << endl;
+  };
+};
+
 int* gauss(int data[], int dataConst[], int size) {
-	const int initLine = 3;
 	int initx1[initLine][initLine]; // init martix
-	int xxxx[initLine];
 	int index = 0;
 	int index2 = 0;
 	for (int i = 0; i < initLine; i++) {
@@ -76,46 +86,70 @@ int* gauss(int data[], int dataConst[], int size) {
 	for (int i = 1; i <= initLine; i++) {
 		cout << dataConst[i - 1] << endl;
 	};
-	// action from 1
-	for (int i = 1; i < initLine; i++) {
-		xxxx[i] = data[initLine*i] / data[0];
-		//xxxx = [2/1, 4/1]
-	};
-	//for (int i = 1; i < initLine; i++) {
-	//	for (int j = i; j < initLine; j++) {
-	//		for (int k = 0; k < initLine; k++) {
-	//			int result = initx1[j][k] - (initx1[i - 1][k] * (initx1[j][i-1]/initx1[i-1][i-1]));
-	//			cout << result << " ";
-	//			//initx1[j][k] = result;
-	//			index2++;
-	//			if (!(index2 % 3)) {
-	//				cout << endl;
-	//			};
-	//		};
-	//	};
-	//		//initx1[i][j] = initx1[i][j];
-	//};
+  cout << "result martix." << endl;
 	int z = 0;
-	int arr[initLine][initLine] = {};
-	for (int i = 1; i < initLine; i++) {
-		for (int j = i; j < initLine; j++) {
-			for (int k = 0; k < initLine; k++){
-				//initx1[j][k] - initx1[z][z];
-				cout << initx1[j][k] - initx1[z][k]*(initx1[j][z] / initx1[z][z]) << " ";
+  int n = 0;
+  int arr[initLine][initLine];
+  int constantArr[initLine];
+  int diffrenceMultiple;
+  int addMultiple;
+	for (int i = 1; i < initLine; i++) { // 1 2
+		for (int j = i; j < initLine; j++) { // 1 2 2
+			for (int k = 0; k < initLine; k++){ /// 0 1 2 0 1 2 0 1 2
+        // initx1[z][k]*(initx1[j][z] / initx1[z][z]): diffrence multiple
+        diffrenceMultiple = (initx1[j][z] / initx1[z][z]);
+        addMultiple = diffrenceMultiple * initx1[z][k];
+        arr[n][k] = initx1[j][k] - addMultiple;
 			};
+      for(int u = 0; u < initLine; u++) {
+        initx1[j][u] = arr[n][u];
+      };
+      constantArr[j] = dataConst[j] - dataConst[z] * diffrenceMultiple;
+      n++;
 		};
-
+    // for(int l = 1; l < initLine; l++) {
+    //   dataConst[l] = constantArr[l];
+    // };
+    for(int l = i; l < initLine; l++) {
+      dataConst[l] = constantArr[l];
+    };
 		z++;
-			//initx1[i][j] = initx1[i][j];
 	};
-	/*for (int i = initLine-1; i > 0; i--) {
-		for (int j = i; j > 0; j--) {
-			n++;
-			cout << z;
-		};
-		z++;
-		n = 0;
-	};*/
+  showMatrix(initx1, initLine);
+  cout << "result matrix constant." << endl;
+  for(int i = 0; i < initLine; i++) {
+    cout << dataConst[i] << endl;
+  };
+
+  int result[initLine];
+  int combine;
+  int initIndex = initLine;
+  cout << "result untie." << endl;
+  // for(int i = initLine-1; i >= 0; i--) {
+  //   for(int j = 0; j < initLine; j++) {
+  //     if(i + 1 == initLine) {
+  //       dataConst[i] / initx1[i][i];
+  //       cout << dataConst[i] / initx1[i][initLine-1];
+  //     };
+  //     // cout << initx1[i][j] << " ";
+  //   };
+  //   cout << endl;
+  // };
+  for(int i = initLine-1; i >= 0; i--) {
+    initIndex--;
+    if(i + 1 == initLine) {
+      result[i] = dataConst[i] / initx1[i][i];
+      cout << dataConst[i] / initx1[i][initLine-1] << endl;
+    } else {
+      // result[i] = 
+      // dataConst[i] - initx1[i][]
+      for(int j = initLine-1; j >= 0; j--) {
+        if(initIndex == j) {
+          break;
+        };
+      };
+    }
+  };
 	/*
 		[
 			[1, -2, -1],
@@ -136,9 +170,9 @@ int main() {
 };
 /*
 		[
-			[1, -2, -1],
-			[2, -3,  1],
-			[4, -2,  7]
+			[1, -2, -1], [-2]
+			[2, -3,  1], [-5]
+			[4, -2,  7], [-7]
 		]
 	1 -2 -1     -2
 	2 -3  1     -5
@@ -151,7 +185,7 @@ int main() {
 	1 -2 -1     -2
 	0 1  3      -1
 	0 0  -7     7
-
+ 
 	1 2 -1
 */
 
